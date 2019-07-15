d25d77ecd2c0419a902b4d6d4181a18d928cb8ad
real-yqdt@163.com <real-yqdt@163.com>
2019-07-15 20:35:05 +0800

    ss

---
+++ src/main/resources/static/ajax/libs/ueditor/assets/jquert-top-drap/css/sweet-alert.css
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta HTTP-EQUIV="pragma" CONTENT="no-cache">
+    <meta HTTP-EQUIV="Cache-Control" CONTENT="no-cache, must-revalidate">
+    <meta HTTP-EQUIV="expires" CONTENT="0">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="Description" content="综合评估微信、微博以及其它移动互联网渠道的价值标准体系,秉承独立公正传统出品,公认最早最权威"/>
+    <meta name="Keywords" content="新榜,新媒体,排行榜,徐达内,xmt,xudanei,xdn,xdnphb,微信排行榜,榜哥,榜妹,看榜,看榜科技" />
+    <title>页面错误</title>
+    <link rel="stylesheet" href="https://assets.newrank.cn/assets/common/css/common.css?t=1562912328839"/>
+
+</head>
+<body>
+<div id="header"></div>
+<div id="header-search"></div>
+
+<div style="margin: auto;margin-top: 78px;width: 300px;text-align: center;">
+    <div >
+        <img src="./assets/img/other/page_error.png" alt=""/>
+    </div>
+    <div style="font-size: 18px;color:#606060;margin: auto;margin-top: 10px;">
+        <span>发生错误，请点击</span><a href="index.html" style="color:#ff8c00;">返回首页</a>
+    </div>
+</div>
+
+
+<div id="footer"></div>
+
+<script type="text/javascript">
+    window.relativeUrl = '/';
+</script>
+
+<script language="javascript" src="https://assets.newrank.cn/assets/common/js/seajs-config-front.js?t=1562912328839"></script>
+
+<script>
+    seajs.use("./static/error/error");
+</script>
+<div style="display: none;">
+    <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253878005'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1253878005' type='text/javascript'%3E%3C/script%3E"));</script>
+</div>
+
+
+</body>
+</html>
